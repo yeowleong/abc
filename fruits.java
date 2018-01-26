1ff6c30961ff89a7a07f1d9b64de0e0b16fb1ba0
Fuxing Loh <lohfuxing@gmail.com>
2018-01-26 13:12:29 +0800

    Changed x = 2

--- fruits.java
+++ fruits.java
@@ -1,7 +1,7 @@
 public class Test {
 	public static void main(String[] args) {
 		System.out.println("apple");
-		int y = 2;
+		int x = 2;
 		System.out.println("orange");
 		System.out.println("durian");
 		System.out.println("mango");		
