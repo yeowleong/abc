7cfc5852ea36efcfcf94889df8e8c01e1b956c24
LYL <yllee@smu.edu.sg>
2018-01-26 13:13:13 +0800

    edited durian line

--- fruits.java
+++ fruits.java
@@ -3,7 +3,7 @@ public class Test {
 		System.out.println("apple");
 		int y = 2;
 		System.out.println("orange");
-		System.out.println("durian");
+		System.out.println("durian is fun");
 		System.out.println("mango");		
 	}
 }
